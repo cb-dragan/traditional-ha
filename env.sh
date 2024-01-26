7fffe03af549ea067110fbf791c5f2580e2ae5aa
Dragan Bocevski <36728855+DraganBocevski@users.noreply.github.com>
2024-01-26 11:25:57 +0100

    Add agent preliminary, some cleanup

--- env.sh
+++ env.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-#export HAPROXY_IP=$(hostname -i)
-export HAPROXY_IP=172.20.0.5
-export OC_IP=172.20.0.6
-export CLIENT1_IP=172.20.0.7
-export CLIENT2_IP=172.20.0.8
-export BROWSER_IP=172.20.0.9
-
-#export CLIENT1_URL=
-
-
-export DOCKER_IMAGE_OC=cloudbees/cloudbees-core-oc:2.426.3.3
-export DOCKER_IMAGE_CLIENT=cloudbees/cloudbees-core-cm:2.426.3.3
+# Hostnames for Operations Center and Controllers
+# The controllers are in HA mode, listening on a single CLIENTS_URL
+# HAProxy listens for this URL and load balances between the controllers
 export OC_URL=oc.ha
 export CLIENTS_URL=client.ha
 
-# Paths on host for mapped volumes
-export OC_PERSISTENCE=/tmp/jenkins_ha
-export CONTROLLER_PERSISTENCE=/tmp/jenkins_ha_controller1
-export CONTROLLER1_CACHES=/tmp/jenkins_ha_controller1_caches
-export CONTROLLER2_CACHES=/tmp/jenkins_ha_controller2_caches
+# CB CI version for Operations Center and Controllers
+export DOCKER_IMAGE_OC=cloudbees/cloudbees-core-oc:2.426.3.3
+export DOCKER_IMAGE_CLIENT=cloudbees/cloudbees-core-cm:2.426.3.3
+
+# We put static IP addresses for docker containers
+export IP_PREFIX=172.47
+export HAPROXY_IP=$IP_PREFIX.0.5
+export OC_IP=$IP_PREFIX.0.6
+export CLIENT1_IP=$IP_PREFIX.0.7
+export CLIENT2_IP=$IP_PREFIX.0.8
+export AGENT_IP=$IP_PREFIX.0.9
+export BROWSER_IP=$IP_PREFIX.0.10
+
+# Paths on Docker host for mapped volumes
+export PERSISTENCE_PREFIX=/tmp/jenkins_ha
+export OC_PERSISTENCE=$PERSISTENCE_PREFIX/oc
+export CONTROLLER_PERSISTENCE=$PERSISTENCE_PREFIX/controllers
+export CONTROLLER1_CACHES=$PERSISTENCE_PREFIX/controller1_caches
+export CONTROLLER2_CACHES=$PERSISTENCE_PREFIX/controller2_caches
+export AGENT_PERSISTENCE=$PERSISTENCE_PREFIX/ssh-agent1
--- up.sh
+++ up.sh
@@ -11,14 +11,17 @@ if [ ! -d "${OC_PERSISTENCE}" ]; then
   sudo mkdir ${CONTROLLER1_CACHES}
   sudo mkdir ${CONTROLLER_PERSISTENCE}
   sudo mkdir ${OC_PERSISTENCE}
+  sudo mkdir ${AGENT_PERSISTENCE}
   sudo chown -R 1000:1000 ${CONTROLLER2_CACHES}
   sudo chown -R 1000:1000 ${CONTROLLER1_CACHES}
   sudo chown -R 1000:1000 ${CONTROLLER_PERSISTENCE}
   sudo chown -R 1000:1000 ${OC_PERSISTENCE}
+  sudo chown -R 1000:1000 ${AGENT_PERSISTENCE}
   sudo chmod 700 ${CONTROLLER2_CACHES}
   sudo chmod 700 ${CONTROLLER1_CACHES}
   sudo chmod 700 ${CONTROLLER_PERSISTENCE}
   sudo chmod 700 ${OC_PERSISTENCE}
+  sudo chmod 700 ${AGENT_PERSISTENCE}
 fi
 
 echo Using Docker host IP: ${DOCKER_HOST_IP}
